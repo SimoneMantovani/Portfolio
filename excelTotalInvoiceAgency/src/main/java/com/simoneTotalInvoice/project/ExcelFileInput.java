7471a6bb3c420199d51f411d4f996d9d5e1bf818
Simone Mantovani <107112401+SimoneMantovani@users.noreply.github.com>
2023-06-20 18:05:18 +0200

    Fix bug inputStream
    Corretto bug per conflitto con la cache

--- excelTotalInvoiceAgency/src/main/java/com/simoneTotalInvoice/project/ExcelFileInput.java
+++ excelTotalInvoiceAgency/src/main/java/com/simoneTotalInvoice/project/ExcelFileInput.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Objects;
 
 @Controller
@@ -19,17 +21,19 @@ public class ExcelFileInput {
     @ResponseBody
     public double xlsxUpload(@RequestParam("file") @NotNull MultipartFile file,
                                     @RequestParam("customer_id") int customerId) throws Exception {
-    if (file.isEmpty() == true){
-        throw new FileNotFoundException();
-    }
+        if (file.isEmpty() == true){
+            throw new FileNotFoundException();
+        }
 
-    if (Objects.equals(checkExtension(file.getOriginalFilename()), "xlsx")){
-        InputStream inputStream = file.getInputStream();
-        double totalInvoice = ExcelReader.reader((FileInputStream) inputStream, customerId);
-        if(totalInvoice==0) throw new Exception("This customer has no invoice");
-        return totalInvoice;
-    }
-    return 0;
+        if (Objects.equals(checkExtension(file.getOriginalFilename()), "xlsx")){
+            Path tmpFilePath = Files.createTempFile("invoices", ".xlsx");
+            file.transferTo(tmpFilePath);
+            FileInputStream stream = new FileInputStream(String.valueOf(tmpFilePath));
+            double totalInvoice = ExcelReader.reader(stream, customerId);
+            if(totalInvoice==0) throw new Exception("This customer has no invoice");
+            return totalInvoice;
+        }
+        return 0;
     }
 
     private String checkExtension(String fileName){
